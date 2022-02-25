8c37d00d808f9f40834956719119cf1435e83353
刘晓亮 <1361149307@qq.com>
2022-02-25 23:39:29 +0800

    测试sentinel

--- cloudalibaba-nacos-sentinel-consumer8801/src/main/java/com/ddcode/sentinel/controller/ConsumerController.java
+++ cloudalibaba-nacos-sentinel-consumer8801/src/main/java/com/ddcode/sentinel/controller/ConsumerController.java
@@ -1,6 +1,7 @@
 package com.ddcode.sentinel.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.ddcode.springcloud.entities.CommonResult;
 import com.ddcode.springcloud.entities.Payment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,7 @@ public class ConsumerController {
 
 
     @GetMapping(value = "/sentinel/nacos/consumer/{id}")
+    @SentinelResource(fallback = "sentinelFallback")
     public String fallback(@PathVariable Long id) {
         String response = restTemplate.getForObject("http://"+ BASE_URL + "/sentinel/nacos/provider/" + id, String.class);
         if (id == 4) {
@@ -29,4 +31,63 @@ public class ConsumerController {
 
         return response;
     }
+
+    public String sentinelFallback(@PathVariable Long id, Throwable throwable) {
+        return "进入运行异常兜底方法 sentinelFallback , id:" + id + ", 异常信息 :" + throwable.getMessage();
+    }
+
+
+    @GetMapping(value = "/sentinel/nacos/consumer2/{id}")
+    @SentinelResource(value = "sentinelFallback1", blockHandler = "sentinelBlockHandler")
+    public String fallback2(@PathVariable Long id) {
+        String response = restTemplate.getForObject("http://"+ BASE_URL + "/sentinel/nacos/provider/" + id, String.class);
+        if (id == 4) {
+            throw new IllegalArgumentException ("IllegalArgumentException,非法参数异常....");
+        }
+        return response;
+    }
+
+    public String sentinelBlockHandler(@PathVariable Long id, BlockException blockException) {
+        return "进入触发sentinel规则的兜底方法 sentinelBlockHandler , id:" + id + ", 异常信息 :" + blockException.getMessage();
+    }
+
+
+    @GetMapping(value = "/sentinel/nacos/consumer3/{id}")
+    @SentinelResource(value = "sentinelFallback2", blockHandler = "sentinelBlockHandler2", fallback = "sentinelFallback2")
+    public String fallback3(@PathVariable Long id) {
+        String response = restTemplate.getForObject("http://"+ BASE_URL + "/sentinel/nacos/provider/" + id, String.class);
+        if (id == 4) {
+            throw new IllegalArgumentException ("IllegalArgumentException,非法参数异常....");
+        }
+        return response;
+    }
+
+    public String sentinelBlockHandler2(@PathVariable Long id, BlockException blockException) {
+        return "进入触发sentinel规则的兜底方法 sentinelBlockHandler2 , id:" + id + ", 异常信息 :" + blockException.getMessage();
+    }
+
+    public String sentinelFallback2(@PathVariable Long id, Throwable throwable) {
+        return "进入运行异常兜底方法 sentinelFallback , id:" + id + ", 异常信息 :" + throwable.getMessage();
+    }
+
+
+    @GetMapping(value = "/sentinel/nacos/consumer4/{id}")
+    @SentinelResource(value = "sentinelFallback3", blockHandler = "sentinelBlockHandler3", fallback = "sentinelFallback3",
+            exceptionsToIgnore = IllegalArgumentException.class)
+    public String fallback4(@PathVariable Long id) {
+        String response = restTemplate.getForObject("http://"+ BASE_URL + "/sentinel/nacos/provider/" + id, String.class);
+        if (id == 4) {
+            throw new IllegalArgumentException ("IllegalArgumentException,非法参数异常....");
+        }
+        return response;
+    }
+
+    public String sentinelBlockHandler3(@PathVariable Long id, BlockException blockException) {
+        return "进入触发sentinel规则的兜底方法 sentinelBlockHandler2 , id:" + id + ", 异常信息 :" + blockException.getMessage();
+    }
+
+    public String sentinelFallback3(@PathVariable Long id, Throwable throwable) {
+        return "进入运行异常兜底方法 sentinelFallback , id:" + id + ", 异常信息 :" + throwable.getMessage();
+    }
+
 }
